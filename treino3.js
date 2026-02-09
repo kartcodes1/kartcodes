c3bd4a73030b50a2fefbe35698ece24b1d23d7d8
kartcodes1 <adrianviniciusrodriguesdias@gmail.com>
2026-02-09 13:47:53 -0300

    change: treino3.js

--- treino3.js
+++ treino3.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // função para calcular as raízes de uma equação quadrática
 function calcularRaizes (a,b,c,) {
     let Delta = b**2 - 4*a*c; // calculo do delta
@@ -9,18 +8,4 @@ let raiz1 = ( - b + Math.sqrt(Delta)) / (2*a); // calculo da raiz 1
 let raiz2 = ( - b - Math.sqrt(Delta)) / (2*a); //calculo da raiz 2
 return [raiz1, raiz2]; // retorna as raízes em um array
 
-} console.log (calcularRaizes(1, 0, 0)); // exemplo de uso da função, deve retornar as raízes 0 e -0, não sei como pode existir uma raiz negativa de zero, mas aconteceu, e aparentemente é possivel
-=======
-// função para calcular as raízes de uma equação quadrática
-function calcularRaizes (a,b,c,) {
-    let Delta = b**2 - 4*a*c; // calculo do delta
-    if (Delta < 0) {
-         return "Não existem raízes reais";}  // se delta for negativo, não existem raízes reais, támbem pode ser representado como "raizes complexas"
-    
-let raiz1 = ( - b + Math.sqrt(Delta)) / (2*a); // calculo da raiz 1
-let raiz2 = ( - b - Math.sqrt(Delta)) / (2*a); //calculo da raiz 2
-return [raiz1, raiz2]; // retorna as raízes em um array
-
-
-} console.log (calcularRaizes(1, 0, 0)); // exemplo de uso da função, deve retornar as raízes 0 e -0
->>>>>>> a1b4915cfdf4fc5d5578f96e3c55a14a8b1a67ac
+} console.log (calcularRaizes(1, 0, 0)); // exemplo de uso da função, deve retornar as raízes 0 e -0, não sei como pode existir uma raiz negativa de zero, mas aconteceu, e aparentemente é possivel.
