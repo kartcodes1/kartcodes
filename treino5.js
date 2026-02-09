74e2e570e05fdce8a2b1f7238802be4da9ea0fd9
kartcodes1 <adrianviniciusrodriguesdias@gmail.com>
2026-02-09 14:17:22 -0300

    change treino5.js

---
+++ treino5.js
@@ -0,0 +1,7 @@
+function aOP(a,b) {
+    return (a + b) / 2;
+} 
+let result = aOP(10, 5);
+console.log ("A média é: " + result);
+if (result < 7) {console.log ( " Reprovado")}
+ if (result >= 7) {console.log ( " Aprovado")}
